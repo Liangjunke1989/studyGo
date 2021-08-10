a60efdf34efff67b6f6cccdffa13d9bf162d66fe
梁俊可 <liangjunke198909@outlook.com>
2021-08-10 17:59:45 +0800

    202108010_04_panic和recover

---
+++ Day11错误处理/03Panic和Recover.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	/*
+		try...catch...finally
+
+		defer：延迟
+		panic:恐慌
+			让当前的一条协程进入恐慌，中断程序的执行。
+		recover:恢复
+			让程序恢复，必须在defer函数中执行。
+
+		go语言没有异常机制，它不能抛出异常，而是使用了panic和recover机制。--->把它作为最后的手段来使用。
+	*/
+}
